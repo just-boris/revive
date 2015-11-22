dca548e2822721465b607ab2100af1f6fc63b397
just-boris <just-boris@hotmail.com>
2015-11-22 23:54:17 +0300

    more compact build

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,21 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const PRODUCTION = process.env.PRODUCTION;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PRODUCTION = NODE_ENV === 'production';
 
 function makeConfig(hotload) {
   return {
     entry: ['./src/application/index.jsx'],
     output: {
       path: path.join(__dirname, 'build'),
-      pathinfo: true,
+      pathinfo: !PRODUCTION,
       filename: 'app.js'
     },
     module: {
       loaders: [{
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|application\/data/,
         loader: 'babel-loader'
       }, {
         test: /\.(png|svg|jpg)$/,
@@ -33,15 +34,19 @@ function makeConfig(hotload) {
     devtool: 'source-map',
     plugins: (() => {
       const plugins = [
+        new webpack.IgnorePlugin(/locale/, /moment/),
+        new webpack.DefinePlugin({
+            MOCK_REQUEST: !PRODUCTION,
+            'process.env': {
+              NODE_ENV: JSON.stringify(NODE_ENV)
+            }
+        }),
         new HtmlWebpackPlugin({
           template: './src/index.html',
           inject: 'body'
         }),
         new ExtractTextPlugin('styles.css'),
-        new webpack.DefinePlugin({
-            MOCK_REQUEST: !PRODUCTION
-        })
-      ];
+    ];
       if(PRODUCTION) {
           plugins.push(new webpack.optimize.UglifyJsPlugin())
       }
