4498efdf4aac33e6009f8a337425bde70d8570f8
just-boris <just-boris@yandex-team.ru>
2015-12-02 14:30:46 +0300

    make link absolute, fixes #2

--- src/application/components/header/Header.jsx
+++ src/application/components/header/Header.jsx
@@ -1,10 +1,9 @@
 import './styles.css';
-import { Component } from 'react';
 import { Link } from 'react-router';
 
 export default function Header() {
     return (<div className="header">
         <Link className="header__brand header__link" to="/">Revive</Link>
-        <Link className="header__link" to="https://github.com/just-boris/revive">Code on Github</Link>
+        <a className="header__link" href="https://github.com/just-boris/revive">Code on Github</a>
     </div>);
 }
